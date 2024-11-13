f87d79aba633e31bdcfd86f2cfbf614c1f661241
AAKASHEE <aakashpatra253@gmail.com>
2024-11-13 22:55:06 +0530

    added last ones

---
+++ lib/BUILD.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Define the initial route correctly
+
+      // Theme settings
+      themeMode: ThemeMode.light, // Use light mode only
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: "/login",
+
+      // Define your named routes
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
